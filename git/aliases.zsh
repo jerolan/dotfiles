da3c318cf7b0bebcb964553b0272af0da328b75b
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-30 13:19:14 -0500

    updated git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,9 +3,9 @@
 alias git='hub'
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
