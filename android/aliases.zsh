0fde0e0bf56d685a591822c35838c4a9ebf1cfb9
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-30 13:12:38 -0500

    added android aliases

---
+++ android/aliases.zsh
@@ -0,0 +1,7 @@
+# android sdk
+alias avdl='emulator -list-avds'
+alias avd5x='~/Library/Android/sdk/tools/emulator @Nexus_5X_API_27'
+
+# react-native development
+alias rndm='adb shell input keyevent 82'
+alias rnpf='adb reverse tcp:8081 tcp:8081'
