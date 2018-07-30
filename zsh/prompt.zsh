408d8b78ce537486430b0bb92a133c2a8a4dcc02
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-30 11:45:15 -0500

    updated promopt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,9 +54,10 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n%{$fg[magenta]%}%n%{$reset_color%} at $(directory_name) $(git_dirty)$(need_push)\n› '
+
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT=" %{$fg_bold[cyan]%}%{$reset_color%}"
 }
 
 precmd() {
