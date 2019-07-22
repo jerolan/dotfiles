a63b75266f6d53281eb0d2c4c20f69c46132efda
jerolan <jerome.olvera@gmail.com>
2019-07-21 20:10:00 -0500

    Update prompt color

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,7 +54,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n%{$fg[magenta]%}%n%{$reset_color%} at $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n%{$fg[yellow]%}%n%{$reset_color%} at $(directory_name) $(git_dirty)$(need_push)\n› '
 
 set_prompt () {
   export RPROMPT=" %{$fg_bold[cyan]%}%{$reset_color%}"
