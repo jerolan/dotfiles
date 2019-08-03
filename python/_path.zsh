eec5b27266979585465e305fb2f9ee9e0de2dd07
jerolan <jerome.olvera@gmail.com>
2019-08-03 12:46:42 -0500

    Remove python zsh config

--- python/_path.zsh
+++
@@ -1,4 +0,0 @@
-# export WORKON_HOME=$HOME/.virtualenvs
-# export PROJECT_HOME=$HOME/testspace
-# export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-# source /usr/local/bin/virtualenvwrapper.sh
