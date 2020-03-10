9e0c1e7dc8b78d8275023b357d4a032d0ec79b51
jerolan <jerome.olvera@gmail.com>
2020-03-10 14:38:01 -0600

    fix OBJC_DISABLE_INITIALIZE_FORK_SAFETY

--- ruby/_path.zsh
+++ ruby/_path.zsh
@@ -1,4 +1,6 @@
-source $HOME/.rvm/scripts/rvm
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM$
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
