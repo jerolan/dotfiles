b79b2177f3d77497d41a095b0dc72027e2a85b95
jerolan <jerome.olvera@gmail.com>
2020-04-23 12:20:53 -0500

    Remove rvm

--- install.sh
+++ install.sh
@@ -16,9 +16,5 @@ ln -sf $PWD/zsh/zshrc ~/.zshrc
 # Install python virtualenv
 pip install virtualenv
 
-# Install rvm
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
-
 # Install Rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- ruby/_path.zsh
+++
@@ -1,6 +0,0 @@
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM$
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
