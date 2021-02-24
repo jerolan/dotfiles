25c2e5932ed5b12180c294d745d061e9c8708688
Jerome Olvera <jolveras@gbm.com.mx>
2021-02-24 09:34:34 -0600

    Fix ruby path

--- ruby/_path.zsh
+++ ruby/_path.zsh
@@ -1,5 +1,4 @@
-# export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 # Load rbenv automatically by appending
 # the following to ~/.zshrc:
-
 eval "$(rbenv init -)"
