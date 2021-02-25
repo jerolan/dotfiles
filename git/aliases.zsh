0b5242e78b64ba78be2fafd90563cf11f4b29174
Jerome Olvera <jolveras@gbm.com.mx>
2021-02-25 16:45:56 -0600

    Add git fetch alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,5 @@
 # The rest of my fun git aliases
+alias gfa='git fetch --all'
 alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
@@ -6,11 +7,11 @@ alias gp='git push'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias gac='git add -A && git commit -m'
+alias gb='git branch'
 alias gc='git commit'
 alias gca='git commit -a'
-alias gco='git checkout'
 alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gco='git checkout'
 alias ge='git-edit-new'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
