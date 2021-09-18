5288079af4ece155795e5affad66b92357f8bcb1
Jerome Olvera <jolveras@gbm.com.mx>
2021-09-17 20:18:49 -0500

    remove conditional

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,9 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
 hub_path=$(which hub)
-if (($ + commands[hub])); then
-  alias git=$hub_path
-fi
+# if (($ + commands[hub])); then
+alias git=$hub_path
+# fi
 
 # The rest of my fun git aliases
 alias gfa='git fetch --all'
