4012ef3293a4ddae79b9fd981fcd5a69344a91bd
Jerome Olvera <jerome.olvera@gmail.com>
2021-09-25 23:18:31 -0500

    use the correct brewfile to each ows

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 set -e
 
+# find the installers and run them iteratively
+find . -name install.sh | while read installer; do sh -c "${installer}"; done
+
 brew tap Homebrew/bundle
-brew bundle
+
+# Install the correct homebrew for each OS type
+if test "$(uname)" = "Darwin"; then
+  brew bundle
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
+  brew bundle --file=Brewfile.linux
+fi
 
 mkdir ~/.config/mvim
 
@@ -12,6 +21,3 @@ ln -sf $PWD/git/gitconfig ~/.gitconfig
 ln -sf $PWD/gradle/gradle.properties ~/.gradle/gradle.properties
 ln -sf $PWD/hyper/hyper.js ~/.hyper.js
 ln -sf $PWD/zsh/zshrc ~/.zshrc
-
-# find the installers and run them iteratively
-find . -name install.sh | while read installer; do sh -c "${installer}"; done
