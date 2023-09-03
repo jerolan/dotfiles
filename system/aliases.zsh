0fe266a00d56c1a2d03723e281f9a379ad07a8f7
Jerome Olvera <jerome.olvera@gmail.com>
2023-09-02 23:27:02 -0600

    update aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,10 @@
 alias lip="ipconfig getifaddr en0" # Depends on your current network adapter
+alias cat="bat"
 
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
+if $(gls &>/dev/null); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
