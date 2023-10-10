44d04b7cdf351ad4eadbbee4d367aa00e524ba05
Jerome Olvera <jolveras@gbm.com.mx>
2023-10-10 09:38:23 -0600

    add bun ext

--- node/path.zsh
+++ node/path.zsh
@@ -3,3 +3,10 @@ eval "$(fnm env)"
 
 # setup github copilot
 eval "$(github-copilot-cli alias -- "$0")"
+
+# bun completions
+[ -s "/Users/42430/.bun/_bun" ] && source "/Users/42430/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
