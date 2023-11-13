181b71c2f24bcb423abe4214e7b19fbab2a2f182
Jerome Olvera <jolveras@gbm.com.mx>
2023-11-13 10:29:59 -0600

    set font size 14

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -7,7 +7,7 @@ module.exports = {
     updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: "Liga SFMono Nerd Font",
