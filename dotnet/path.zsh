beb43e7a4f49c12c711959d7068071d80caf5579
Jerome Olvera <jerome.olvera@gmail.com>
2024-01-01 19:31:17 -0600

    add dotnet-core-uninstall to path

--- dotnet/path.zsh
+++ dotnet/path.zsh
@@ -1,2 +1,2 @@
-export PATH="$PATH:$HOME/.dotnet/tools:/usr/local/share/dotnet"
+export PATH="$PATH:$HOME/.dotnet/tools:/usr/local/share/dotnet:$HOME/dotnet-core-uninstall"
 export DOTNET_ROOT="$(brew --prefix)/opt/dotnet/libexec"
