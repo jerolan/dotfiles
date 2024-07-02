246ea9a91a0713af1b3d17cd61008ccb7ba5c814
Jerome Olvera <jerome.olvera@gmail.com>
2024-07-02 12:31:27 -0600

    Update font size to 18 pixels

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -7,7 +7,7 @@ module.exports = {
     updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 18,
 
     // font family with optional fallbacks
     fontFamily: "'Fantasque Sans Mono', monospace",
