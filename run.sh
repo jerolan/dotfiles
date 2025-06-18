941c828a2513d76cfa3bdef279858432be31496a
Jerome Olvera <jerome.olvera@gmail.com>
2025-06-18 11:41:20 -0600

    Remove gradle.properties symlink from run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,5 @@ brew bundle
 # Link System files
 ln -sf $PWD/git/gitignore ~/.gitignore
 ln -sf $PWD/git/gitconfig ~/.gitconfig
-ln -sf $PWD/gradle/gradle.properties ~/.gradle/gradle.properties
 ln -sf $PWD/hyper/hyper.js ~/.hyper.js
 ln -sf $PWD/zsh/zshrc ~/.zshrc
