9bbdc4595b98f6bc9bcb9d2129d801a1ec3d64d4
Jerome Olvera <jolveras@gbm.com.mx>
2025-08-21 10:44:46 -0600

    Refactor antigen config to remove unnecessary bundles and enhance autosuggestions handling

--- antigen/config.zsh
+++ antigen/config.zsh
@@ -1,11 +1,17 @@
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle command-not-found
-
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-autosuggestions
 
 # Tell Antigen that you're done.
 antigen apply
+
+# remove OMZ hooks and any custom title hooks
+typeset -ga precmd_functions preexec_functions
+precmd_functions=(${precmd_functions:#omz_termsupport_precmd} ${precmd_functions:#title})
+preexec_functions=(${preexec_functions:#omz_termsupport_preexec} ${preexec_functions:#title})
+function title() { :; }  # no-op
+
+# rebind autosuggestions only once
+(( $+functions[_zsh_autosuggest_bind_widgets] )) && _zsh_autosuggest_bind_widgets
