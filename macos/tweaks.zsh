a0be6c7374d2325962d7090c86b301c544e3d5dc
Jerome Olvera <jerome.olvera@gmail.com>
2026-01-04 18:24:26 -0600

    Remove Safari development settings from tweaks.zsh

--- macos/tweaks.zsh
+++ macos/tweaks.zsh
@@ -5,12 +5,5 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
 # Disable Credential Security Support Provider (CredSSP) authentication for Microsoft Remote Desktop.
 defaults write com.microsoft.rdc.macos ClientSettings.EnforceCredSSPSupport 0
