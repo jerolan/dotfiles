2000a0b1ef59a81fe40181207963f204edbac4e6
Jerome Olvera <jerome.olvera@gmail.com>
2026-02-02 21:43:24 -0600

    Refactor Homebrew installation script for improved clarity and efficiency

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew); then
-  echo "  Installing Homebrew for you."
+if command -v brew >/dev/null 2>&1; then
+  exit 0
+fi
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+OS="$(uname)"
+if [ "$OS" = "Darwin" ] || [ "$OS" = "Linux" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
